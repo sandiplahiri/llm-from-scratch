eb92b816e8701de320f9d542b841e8cea5483aea
sandiplahiri <sandips.webmail@gmail.com>
2026-04-28 14:51:33 -0400

    Initial commit.

---
+++ multi_head_attention/multi_head_attention.py
@@ -0,0 +1,89 @@
+
+from matplotlib.pylab import rint
+import torch
+from torch import nn
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, 
+                 d_in, 
+                 d_out,
+                 context_length,
+                 dropout,
+                 num_heads,
+                 qkv_bias=False):
+        super().__init__()
+        assert(d_out % num_heads == 0), "d_out must be divisible by num_heads"
+
+        self.d_out     = d_out
+        self.num_heads = num_heads
+        self.head_dim  = d_out // num_heads
+        self.W_query   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key     = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj  = nn.Linear(d_out, d_out)
+        self.dropout   = nn.Dropout(dropout)
+
+        self.register_buffer("mask",
+                             torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        ) 
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+
+        keys    = self.W_key(x)
+        queries = self.W_query(x)
+        values  = self.W_value(x)
+        # Reshapes the keys, queries, and values to have the shape (batch_size, num_tokens, num_heads, head_dim)
+        # Implicitly splits the matrix by adding a num_heads dimension.
+        # Then unrolls the last dim: (b, num_tokens, d_out) to (b, num_tokens, num_heads, head_dim)
+        keys    = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        values  = values.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # Transpose from 
+        # shape(b, num_tokens, num_heads, head_dim) to 
+        # shape(b, num_heads, num_tokens, head_dim)
+        # This transposition is crucial for correctly aligning the queries, keys, and values 
+        # across difference heads and performing batched matrixed multilplications effectively
+        keys    = keys.transpose(1,2)
+        queries = queries.transpose(1,2)
+        values = values.transpose(1,2)
+
+        # Computes the dot product for each head
+        # The matrix multiplication implementation of PyTorch handles the found-dimensional input tensor
+        # so that the matrix multiplication is carried out between two last dimensions (num_tokenn, head_dim)
+        # and then repeated for the individual heads
+        attn_scores = queries @ keys.transpose(2,3)
+
+        # Applies causal masking by setting the upper triangular part of attn_scores to -inf
+        # Truncates the masks of size context_length x context_length to the
+        # number of tokens in the input sequence, i.e., to num_tokens x num_tokens
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+        # Uses the mask to fill attention scores
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        # Normalizes the attention scores using softmax
+        attn_weights = torch.softmax(attn_scores/keys.shape[-1]**0.5, dim=-1)
+
+        # Applies dropout to the attention weights
+        attn_weights = self.dropout(attn_weights)
+
+        # Computes the weighted sum of the values using the attention weights
+        # Tensor shape: (b, num_heads, num_tokens, head_dim)
+        context_vec = attn_weights @ values
+         # Tensor shape: (b, num_tokens, num_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(1,2) 
+
+        # Combines heads, where self_d_out = self.num_heads * self_head_dim
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        print("[MultiHeadAttention] context_vec before optional projection:", context_vec)
+        print("[MultiHeadAttention] context_vec shape before optional projection:", context_vec.shape)
+
+
+        # Adds an optional linear projection
+        context_vec = self.out_proj(context_vec)
+        print("[MultiHeadAttention] context_vec after optional projection:", context_vec)
+        print("[MultiHeadAttention] context_vec shape after optional projection:", context_vec.shape)
+
+        return context_vec
---
+++ multi_head_attention/test.py
@@ -0,0 +1,40 @@
+import torch
+from multi_head_attention import MultiHeadAttention
+
+
+# Instantiate an example text where each token is represented by a 3-dimensional embedding vector
+inputs = torch.tensor(
+                      [[0.43, 0.15, 0.89], # Your      (x^1)
+                       [0.55, 0.87, 0.66], # journey]  (x^2)
+                       [0.57, 0.85, 0.64], # starts    (x^3)
+                       [0.22, 0.58, 0.33], # with      (x^4)
+                       [0.77, 0.25, 0.10], # one       (x^5)
+                       [0.05, 0.80, 0.55] # step       (x^6)
+                     ]
+                   )
+
+# Create a two item batch by stacking the same inputs tensor
+# How the dim parameter Affects Shape
+# If you have N tensors, each of shape (A, B):
+# a. dim=0: The new dimension is added at the beginning.
+#    Resulting shape: (N, A, B)
+# b. dim=1: The new dimension is added in the middle.
+#    Resulting shape: (A, N, B) 
+# c. dim=2 (or dim=-1): The new dimension is added at the end.
+#    Resulting shape: (A, B, N) 
+batch = torch.stack((inputs, inputs), dim=0)
+# Expected output: (2, 6, 3 - batch_size, num_tokens, embedding_dim) 
+print("Input batch shape:", batch.shape)   
+
+# torch.manual_seed() in PyTorch sets a fixed seed for the random number generator (RNG) 
+# across all devices (CPU and CUDA), ensuring reproducible results. 
+# By initializing the RNG to a specific state, it guarantees that random operations—such 
+# as weight initialization or data shuffling—produce the exact same sequence each time the script is executed.
+torch.manual_seed(123)
+batch_size, context_length, d_in = batch.shape
+d_out = 2
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+context_vecs = mha(batch)
+print(context_vecs)
+print("context vectors shape:", context_vecs.shape)
+
