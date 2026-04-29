30854aa3ed1c89b2933b3e28d549b1c8adfa769c
sandiplahiri <sandips.webmail@gmail.com>
2026-04-29 17:58:05 -0400

    Added missed TransformerBlock from the earlier commits.formerBlock

---
+++ transformer_block/transformer_block.py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+from multi_head_attention import MultiHeadAttention
+from .feed_forward_nn import FeedForward
+from .layer_norm import LayerNorm
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+
+        super().__init__()
+        self.att = MultiHeadAttention(
+                                      d_in=cfg["emb_dim"],
+                                      d_out=cfg["emb_dim"],
+                                      context_length=cfg["context_length"],
+                                      num_heads=cfg["n_heads"],
+                                      dropout=cfg["drop_rate"],
+                                      qkv_bias=cfg["qkv_bias"]
+                                     )
+        
+        self.ff            = FeedForward(cfg)
+        self.norm1         = LayerNorm(cfg["emb_dim"])
+        self.norm2         = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+
+    def forward(self, x):
+
+        # Shortcut connection for attention block
+        shortcut = x
+
+        # Step 1: Normalize
+        x = self.norm1(x) 
+
+        # Step 2: Multi-head self attention
+        x = self.att(x)
+
+        # Step 3: Dropout
+        x = self.drop_shortcut(x)
+
+        # Step 4: Add the shortcut connection
+        x = x + shortcut
+
+        # Shortcut connection for the feed forward block
+        shortcut = x
+
+        # Step 5: Normalize
+        x = self.norm2(x)
+
+        # Step 6: Feed forward network
+        x = self.ff(x)
+
+        # Step 7: Dropout
+        x = self.drop_shortcut(x)
+
+        # Step 8: Add the shortcut connection
+        x = x + shortcut
+
+        return x
